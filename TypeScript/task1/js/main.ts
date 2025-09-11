6eb6cf1d541129c2571653d212f68122672c7edf
Nordine000 <ounissi970@gmail.com>
2025-09-11 11:57:45 +0200

    test

--- TypeScript/task1/js/main.ts
+++ TypeScript/task1/js/main.ts
@@ -1,41 +1,16 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
     location: string;
+    [propName: string]: any;
 }
 
-const student1: Student = {
-    firstName: "b",
-    lastName: "b",
-    age: 12,
-    location: "paris"
+interface Directors extends Teacher {
+    numberOfReports: number;
 }
 
-const student2: Student = {
-    firstName: "a",
-    lastName: "a",
-    age: 18,
-    location: "paris"
+function printTeacher(firstName: string, lastName: string): string {
+    return firstName.charAt(0) + "." + lastName;
 }
-const studentList: Student [] = [student1, student2]
-
-const table = document.createElement("table");
-const tbody = document.createElement("tbody");
-
-studentList.forEach((student) => {
-    const row = document.createElement("tr");
-
-    const firstNameCell = document.createElement("td");
-    firstNameCell.textContent = student.firstName;
-
-    const locationCell = document.createElement("td");
-    locationCell.textContent = student.location;
-
-    row.appendChild(firstNameCell);
-    row.appendChild(locationCell);
-    tbody.appendChild(row);
-});
-
-table.appendChild(tbody);
-document.body.appendChild(table);
