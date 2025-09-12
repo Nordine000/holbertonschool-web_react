1fa6220ce4c99e30a5270db1b96e16802d7da5d6
Nordine000 <ounissi970@gmail.com>
2025-09-12 15:22:29 +0200

    test

--- TypeScript/task_3/js/crud.d.ts
+++ TypeScript/task_3/js/crud.d.ts
@@ -2,5 +2,5 @@ import { RowID, RowElement } from './interface.ts';
 
 declare function insertRow(row: RowElement): RowID;
 declare function deleteRow(rowId: RowID): void;
-declare function updateRow(rowIdd: RowElement, rowId: RowID): RowID;
+declare function updateRow(rowId: RowID, rowId: RowElement): RowID;
 
--- TypeScript/task_3/js/main.ts
+++ TypeScript/task_3/js/main.ts
@@ -78,3 +78,18 @@ function teachClass(todayClass: Subject) {
 
 console.log(teachClass("Math"));
 console.log(teachClass("History"));
+
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva",
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+     ...row,
+    age: 23,
+}
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
